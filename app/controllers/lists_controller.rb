0f1de1652807a3903aff88d885c6e1a5b19eaf76
Suzanne Goncalves <suzanne.g.p@hotmail.fr>
2023-02-24 16:13:30 +0100

    Finish

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,20 +1,37 @@
 class ListsController < ApplicationController
+    before_action :set_list, only: [:show, :destroy]
 
-    # resources :list, only: [:index, :new, :show, :create] do
     def index
-        @lists = List.all
+      @lists = List.all
     end
-
+  
+    def show; end
+  
     def new
-        @list = List.new
+      @list = List.new
     end
-
-    def show
-        @list = List.find(params[:id])
-    end
-
+  
     def create
-        @list = List.new(params[:list])
-        @list.save
+      @list = List.new(list_params)
+      if @list.save
+        redirect_to list_path(@list)
+      else
+        render :new, status: :unprocessable_entity
+      end
+    end
+  
+    def destroy
+      @list.destroy
+      redirect_to lists_path, status: :see_other
+    end
+  
+    private
+  
+    def set_list
+      @list = List.find(params[:id])
+    end
+  
+    def list_params
+      params.require(:list).permit(:name)
     end
 end
